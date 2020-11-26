0f10971dae6a5383993b343dcfb0022c8a3dafa6
Ivan Plastun <plastun.ivan2018@yandex.ru>
2020-11-26 18:02:02 +0300

    changed file swiperEvent.js

--- js/swipeEvent.js
+++ js/swipeEvent.js
@@ -1,198 +1,189 @@
-/** 
-    * Функция определения события swipe на элементе.
-    * @param {Object} el - элемент DOM
-    * @param {Object} settings - объект с предварительными настройками
-*/
-const swipe = function (el, setting) {
-    // Настройки по умолчанию
-    const settings = Object.assign({}, {
-        minDist: 60, // Минимальная дистанция, которую должен пройти указатель, чтобы жесть считался как свайп (px)
-        maxDist: 120, // Максимальная дистанция, не превышая которую может пройти указатель, чтобы жест считался как свайп (px)
-        maxTime: 700, // Максимальное время, за которое должен быть совершен свайп(ms)
-        minTime: 50 // Минимальное время, за которое должен быть совершен свайп (ms)
-    }, setting)
+/**
+ * Object.assign() polyfill
+ */
+Object.assign||Object.defineProperty(Object,"assign",{enumerable:!1,configurable:!0,writable:!0,value:function(a,b){"use strict";if(void 0===a||null===a)error("Cannot convert first argument to object");for(var c=Object(a),d=1;d<arguments.length;d++){var e=arguments[d];if(void 0!==e&&null!==e)for(var f=Object.keys(Object(e)),g=0,h=f.length;g<h;g++){var i=f[g],j=Object.getOwnPropertyDescriptor(e,i);void 0!==j&&j.enumerable&&(c[i]=e[i])}}return c}});
 
-    // Коррекция времени при ошибочных значениях
-    if (settings.maxTime < settings.minTime) {
-        settings.maxTime = settings.minTime + 500
-    }
+/**
+ * CustomEvent() polyfill
+ */
+!function(){if("function"==typeof window.CustomEvent)return;function t(t,e){e=e||{bubbles:!1,cancelable:!1,detail:void 0};var n=document.createEvent("CustomEvent");return n.initCustomEvent(t,e.bubbles,e.cancelable,e.detail),n}t.prototype=window.Event.prototype,window.CustomEvent=t}();
 
+
+/**
+ * Функция определения события swipe на элементе.
+ * @param {Object} el - элемент DOM.
+ * @param {Object} settings - объект с предварительными настройками.
+ */
+var swipe = function(el, settings) {
+
+    // настройки по умолчанию
+    var settings = Object.assign({}, {
+        minDist: 60,  // минимальная дистанция, которую должен пройти указатель, чтобы жест считался как свайп (px)
+        maxDist: 120, // максимальная дистанция, не превышая которую может пройти указатель, чтобы жест считался как свайп (px)
+        maxTime: 700, // максимальное время, за которое должен быть совершен свайп (ms)
+        minTime: 50   // минимальное время, за которое должен быть совершен свайп (ms)
+    }, settings);
+
+    // коррекция времени при ошибочных значениях
+    if (settings.maxTime < settings.minTime) settings.maxTime = settings.minTime + 500;
     if (settings.maxTime < 100 || settings.minTime < 50) {
-        settings.maxTime = 700
-        settings.minTime = 50
+        settings.maxTime = 700;
+        settings.minTime = 50;
     }
 
-    let events = null
-    let dir // направление свайпа (horizontal, vertical)
-    let swipeType // тип свайпа (up, down, left, right)
-    let dist // дистанция, пройденная указателем
-    let isMouse = false // поддержка мыши (не используется тач-событий)
-    let isMouseDown = false // указание на активное нажатие мыши (Не используется для тач-событий)
-    let startX = 0 // Начало координат по оси X (pageX)
-    let distX = 0 // Дистанция, пройденная указателем по оси X
-    let startY = 0 // Начало координат по оси Y (pageY)
-    let distY = 0 // Дистанция, пройденная указателем по оси Y
-    let startTime = 0 // Время начала касания
-    let support = { // поддерживаемые браузером типы событий
-        pointer: !!("PointerEvent" in window || ("msPointerEnabled" in window.navigator)),
-        touch: !!(typeof window.orientation !== "undefined" || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || "ontouchstart" in window || navigator.msMaxTouchPoints || "maxTouchPoints" in window.navigator > 1 || "msMaxTouchPoints" in window.navigator > 1)
-    };
+    var dir,                // направление свайпа (horizontal, vertical)
+        swipeType,            // тип свайпа (up, down, left, right)
+        dist,                 // дистанция, пройденная указателем
+        isMouse = false,      // поддержка мыши (не используется для тач-событий)
+        isMouseDown = false,  // указание на активное нажатие мыши (не используется для тач-событий)
+        startX = 0,           // начало координат по оси X (pageX)
+        distX = 0,            // дистанция, пройденная указателем по оси X
+        startY = 0,           // начало координат по оси Y (pageY)
+        distY = 0,            // дистанция, пройденная указателем по оси Y
+        startTime = 0,        // время начала касания
+        support = {           // поддерживаемые браузером типы событий
+            pointer: !!("PointerEvent" in window || ("msPointerEnabled" in window.navigator)),
+            touch: !!(typeof window.orientation !== "undefined" || /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || "ontouchstart" in window || navigator.msMaxTouchPoints || "maxTouchPoints" in window.navigator > 1 || "msMaxTouchPoints" in window.navigator > 1)
+        };
 
     /**
      * Опредление доступных в браузере событий: pointer, touch и mouse.
      * @returns {Object} - возвращает объект с доступными событиями.
-    */
-    const getSupportedEvents = function () {
-
-        switch(true) {
+     */
+    var getSupportedEvents = function() {
+        switch (true) {
             case support.pointer:
                 events = {
-                    type: 'pointer',
-                    start: 'PointerDown',
-                    move: 'PointerMove',
-                    end: 'PointerUp',
-                    cancel: 'PointerCancel',
-                    leave: 'PointerLeave'
+                    type:   "pointer",
+                    start:  "PointerDown",
+                    move:   "PointerMove",
+                    end:    "PointerUp",
+                    cancel: "PointerCancel",
+                    leave:  "PointerLeave"
+                };
+                // добавление префиксов для IE10
+                var ie10 = (window.navigator.msPointerEnabled && Function('/*@cc_on return document.documentMode===10@*/')());
+                for (var value in events) {
+                    if (value === "type") continue;
+                    events[value] = (ie10) ? "MS" + events[value] : events[value].toLowerCase();
                 }
-
-                // Добавление префиксов для IE10
-                let ie10 = (window.navigator.msPointerEnabled && Function('/*@cc_on return document.documentMode===10@*/')())
-                for(let value in events) {
-                    if (value === 'type') continue
-                    events[value] = (ie10) ? 'MS' + events[value] : events[value].toLowerCase()
-                }
-            break
+                break;
             case support.touch:
                 events = {
-                    type: 'touch',
-                    start: 'touchstart',
-                    move: 'touchmove',
-                    end: 'touchend',
-                    cancel: 'touchcancel'
-                }
-            break
+                    type:   "touch",
+                    start:  "touchstart",
+                    move:   "touchmove",
+                    end:    "touchend",
+                    cancel: "touchcancel"
+                };
+                break;
             default:
                 events = {
-                    type: "mouse",
-                    start: 'mousedown',
-                    move: 'mousemove',
-                    end: 'mouseup',
-                    leave: 'mouseleave'
-                }
-            break
+                    type:  "mouse",
+                    start: "mousedown",
+                    move:  "mousemove",
+                    end:   "mouseup",
+                    leave: "mouseleave"
+                };
+                break;
         }
+        return events;
+    };
 
-        return events
-    }
 
     /**
-        * Объединение событий mouse/pointer и touch
-        * @param e {object} - принимает в качестве аргумента событие
-        * @returns {TouchList|e} возвращает либо TouchList, либо оставляет событие без изменения
-    */
-    const eventsUnify = function (e) {
-        return e.changedTouches ? e.changedTouches[0] : e
-    }
+     * Объединение событий mouse/pointer и touch.
+     * @param e {Event} - принимает в качестве аргумента событие.
+     * @returns {TouchList|Event} - возвращает либо TouchList, либо оставляет событие без изменения.
+     */
+    var eventsUnify = function(e) {
+        return e.changedTouches ? e.changedTouches[0] : e;
+    };
+
 
     /**
      * Обрабочик начала касания указателем.
      * @param e {Event} - получает событие.
-    */
-    const checkStart = function (e) {
-        let event = eventsUnify(e)
-
-        if (support.touch && typeof e.touches !== 'undefined' && e.touches.length !== 1) {
-            return //игнорирование касания несколькими пальцами
-        }
-        dir = 'none'
-        swipeType = 'none'
-        dist = 0
-        startX = event.pageX
-        startY = event.pageY
-        startTime = new Date().getTime()
-        if (isMouse) {
-            isMouseDown = true // поддержка мыши
-        }
-    }
+     */
+    var checkStart = function(e) {
+        var event = eventsUnify(e);
+        if (support.touch && typeof e.touches !== "undefined" && e.touches.length !== 1) return; // игнорирование касания несколькими пальцами
+        dir = "none";
+        swipeType = "none";
+        dist = 0;
+        startX = event.pageX;
+        startY = event.pageY;
+        startTime = new Date().getTime();
+        if (isMouse) isMouseDown = true; // поддержка мыши
+    };
 
     /**
      * Обработчик движения указателя.
      * @param e {Event} - получает событие.
-    */
-    const checkMove = function (e) {
-        // Выход из функции, если мышь перестала быть активна во время движения
-        if (isMouse && !isMouseDown) {
-            return
-        }
-
-        let event = eventsUnify(e)
-        distX = event.pageX - startX
-        distY = event.pageY - startY
-        if (Math.abs(distX) > Math.abs(distY)) {
-            dir = (distX < 0) ? 'left' : 'right'
-        } else {
-            dir = (distY < 0) ? 'up' : 'down'
-        }
-    }
+     */
+    var checkMove = function(e) {
+        if (isMouse && !isMouseDown) return; // выход из функции, если мышь перестала быть активна во время движения
+        var event = eventsUnify(e);
+        distX = event.pageX - startX;
+        distY = event.pageY - startY;
+        if (Math.abs(distX) > Math.abs(distY)) dir = (distX < 0) ? "left" : "right";
+        else dir = (distY < 0) ? "up" : "down";
+    };
 
     /**
      * Обработчик окончания касания указателем.
      * @param e {Event} - получает событие.
      */
-    const checkEnd = function (e) {
-        // Выход из функции и сброс проверки нажатия мыши
-        if (isMouse && !isMouseDown) {
-            isMouseDown = false
-            return
+    var checkEnd = function(e) {
+        if (isMouse && !isMouseDown) { // выход из функции и сброс проверки нажатия мыши
+            isMouseDown = false;
+            return;
         }
-        let endTime = new Date().getTime()
-        let time = endTime - startTime
-        // Проверка времени жеста
-        if (time >= settings.minTime && time <= settings.maxTime) {
+        var endTime = new Date().getTime();
+        var time = endTime - startTime;
+        if (time >= settings.minTime && time <= settings.maxTime) { // проверка времени жеста
             if (Math.abs(distX) >= settings.minDist && Math.abs(distY) <= settings.maxDist) {
-                swipeType = dir // Определение типа свайпа как "left" или "right"
+                swipeType = dir; // опредление типа свайпа как "left" или "right"
             } else if (Math.abs(distY) >= settings.minDist && Math.abs(distX) <= settings.maxDist) {
-                swipeType = dir // Определение типа свайпа как "top" или "down"
+                swipeType = dir; // опредление типа свайпа как "top" или "down"
             }
         }
-        dist = (dir === 'left' || dir === 'right') ? Math.abs(distX) : Math.abs(distY) // Определение пройденной указателем дистанции
+        dist = (dir === "left" || dir === "right") ? Math.abs(distX) : Math.abs(distY); // опредление пройденной указателем дистанции
 
-        // Генерация кастомного события swipe
-        if (swipeType !== 'none' && dist >= settings.minDist) {
-            let swipeEvent = new CustomEvent('swipe', {
+        // генерация кастомного события swipe
+        if (swipeType !== "none" && dist >= settings.minDist) {
+            var swipeEvent = new CustomEvent("swipe", {
                 bubbles: true,
                 cancelable: true,
                 detail: {
-                    full: e, // Полное событие Event
-                    dir: swipeType, // направление свайпа
+                    full: e, // полное событие Event
+                    dir:  swipeType, // направление свайпа
                     dist: dist, // дистанция свайпа
                     time: time // время, потраченное на свайп
                 }
-            })
-            el.dispatchEvent(swipeEvent)
+            });
+            el.dispatchEvent(swipeEvent);
         }
-    }
+    };
 
-    // Добавление поддерживаемых событий
-    let  = getSupportedEvents()
+    // добавление поддерживаемых событий
+    var events = getSupportedEvents();
 
-    // Провека наличия мыши
-    if ((support.pointer && !support.touch) || events.type === 'mouse') {
-        isMouse = true
-    }
+    // проверка наличия мыши
+    if ((support.pointer && !support.touch) || events.type === "mouse") isMouse = true;
 
-    // Добавление обработчиков на элемент
-    el.addEventListener(events.start, checkStart)
-    el.addEventListener(events.move, checkMove)
-    el.addEventListener(events.end, checkEnd)
-    if (support.pointer && support.touch) {
-        el.addEventListener('lostpointercapture', checkEnd)
+    // добавление обработчиков на элемент
+    el.addEventListener(events.start, checkStart);
+    el.addEventListener(events.move, checkMove);
+    el.addEventListener(events.end, checkEnd);
+    if(support.pointer && support.touch) {
+        el.addEventListener('lostpointercapture', checkEnd);
     }
-}
+};
 
 const mainElement = document.querySelector('main')
 const sidebar = document.querySelector('.sidebar')
-// const mediaQuery = window.matchMedia('(min-width: 968px)')
+const mediaQuery = window.matchMedia('(min-width: 968px)')
 swipe(mainElement, {maxTime: 1000, minTime: 100, maxDist: 150, minDist: 60})
 
 // Обработка свайпов
@@ -207,9 +198,3 @@ mainElement.addEventListener('swipe', function (e) {
         sidebar.classList.remove('sidebar--active')
     }
 })
-
-const mediaQuery = window.matchMedia('(min-width: 968px)')
- 
-if (mediaQuery.matches) {
-    console.log('Media Query Matched!')
-}
